7bf26305af73122284bcfb5152bb92c1f52faded
Matthew Bauer <mjbauer95@gmail.com>
2018-10-21 17:11:27 -0500

    Cleanup static overlay

--- pkgs/top-level/static.nix
+++ pkgs/top-level/static.nix
@@ -1,7 +1,14 @@
-self: super: {
-  stdenv = (if super.stdenv.isDarwin then (x: x)
-            else super.stdenvAdapters.makeStaticBinaries)
-              (super.stdenvAdapters.makeStaticLibraries super.stdenv);
+self: super: let
+  inherit (super.stdenvAdapters) makeStaticBinaries
+                                 overrideInStdenv
+                                 makeStaticLibraries;
+  inherit (super.lib) foldl optional flip id;
+  staticAdapters = [ makeStaticLibraries ]
+    ++ optional (!super.stdenv.hostPlatform.isDarwin) makeStaticBinaries
+    ++ optional (super.stdenv.hostPlatform.libc == "glibc") ((flip overrideInStdenv) [ super.buildPackages.glibc.static ])
+  ;
+in {
+  stdenv = foldl (flip id) super.stdenv staticAdapters;
   # libiconv = super.libiconv.override {
   #   enableShared = false;
   #   enableStatic = true;
